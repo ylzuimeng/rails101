3e0f48b2d9d35fdbe77550db6dd54550a7b65ed9
杨磊 <yanglei@yangleideMacBook-Air.local>
2016-12-21 15:41:46 +0800

    implement groups#destroy

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -19,10 +19,16 @@ class GroupsController < ApplicationController
   end
      def update
      @group = Group.find(params[:id])
- 
+
      @group.update(group_params)
 
      redirect_to groups_path, notice: "Update Success"
+   end
+   def destroy
+     @group = Group.find(params[:id])
+     @group.destroy
+     flash[:alert] = "Group deleted"
+     redirect_to groups_path
    end
 
   private
